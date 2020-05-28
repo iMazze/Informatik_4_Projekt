07e8600a31fac01bb11b907621d9b248986e0286
Maximilian Klug <klug.maximilia-tfe17@it.dhbw-ravensburg.de>
2020-05-28 13:42:56 +0200

    added unit test

---
+++ Source/complex_calculator/complex_calculator_unittest/unittest1.cpp
@@ -0,0 +1,23 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include "../complex_calculator/Complex.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace complex_calculator_unittest
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: Testcode hier eingeben
+			Complex c;
+			c.getImaginary();
+
+		}
+
+	};
+}
